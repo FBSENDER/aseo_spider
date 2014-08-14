f79dd0412318251d0a584d5234f714e8bda35d5d
mengchen.liu <mengchen.liu@corp.elong.com>
2014-08-14 20:01:01 +0800

    update

--- lib/aseo_spider/amazon_spider/item_lookup.rb
+++ lib/aseo_spider/amazon_spider/item_lookup.rb
@@ -1,27 +1,31 @@
 module AseoSpider
   class Amazon 
     def item_lookup(asin,access_key,secret_key,associate_tag)
-      result = Amazon::Ecs.item_lookup(asin,{:AWS_access_key_id => access_key,:AWS_secret_key => secret_key,:associate_tag => associate_tag})
-      if result.has_error?
-        raise result.error
+      item = nil
+      loop do 
+        begin
+          result = Amazon::Ecs.item_lookup(asin,{:AWS_access_key_id => access_key,:AWS_secret_key => secret_key,:associate_tag => associate_tag})
+          item = result.first_item
+          break
+        rescue Exception => ex
+          puts ex
+        end
       end
-      return nil if result.first_item.nil?
-      item = result.first_item
       hash_result = {}
       hash_result[:title] = item.get("ItemAttributes/Title")
+      hash_result[:img_1] = item.get("SmallImage/URL")
+      hash_result[:img_2] = item.get("MediumImage/URL")
+      hash_result[:img_3] = item.get("LargeImage/URL")
+      hash_result[:url] = item.get("DetailPageURL")
+      hash_result[:brand] = item.get("ItemAttributes/Brand")
       hash_result[:features] = Amazon::Element.get_array(item,"ItemAttributes/Feature")
       hash_result[:descriptions] = item.get("EditorialReviews/EditorialReview/Content")
-      hash_result[:category] = []
-      node_path = "BrowseNodes/BrowseNode"
-      loop do
-        node = item.get(node_path)
-        break if node.nil?
-        category = {:id => item.get("#{node_path}\/BrowseNodeId"),:name => item.get("#{node_path}\/Name")}
-        category[:is_category_root] = item.get("#{node_path}\/IsCategoryRoot") unless item.get("#{node_path}\/IsCategoryRoot").nil?
-        hash_result[:category] << category
-        node_path += '/Ancestors/BrowseNode'
-      end
+      hash_result[:category_id] = item.get("BrowseNodes/BrowseNode/BrowseNodeId")
+      hash_result[:category_name] = item.get("BrowseNodes/BrowseNode/Name")
+      hash_result[:price] =/[\d\/.,]+/.match(item.get("ItemAttributes/ListPrice/FormattedPrice")).to_s.sub(',','').to_f
+      hash_result[:price] = /[\d\/.,]+/.match(item.get("OfferSummary/LowestNewPrice/FormattedPrice")).to_s.sub(',','').to_f if hash_result[:price] == 0
       hash_result
+
     end
   end
 end
